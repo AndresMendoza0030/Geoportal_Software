4e7d0d5cbb3a4dff4364b1d06eb909f6f081e8eb
ErickVasquez11 <00065520@uca.edu.sv>
2024-11-15 12:30:53 -0600

    :bug: Fix a bug Map

--- src/main/java/com/nikolas/leaflet/configuration/SecurityConfig.java
+++ src/main/java/com/nikolas/leaflet/configuration/SecurityConfig.java
@@ -26,7 +26,7 @@ private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/","/map/index","/update","/index", "/map/index", "/register", "/map/unidades", "/css/**", "/js/**", "/images/**")
+            .antMatchers("/","/update","/index", "/map/index", "/register", "/map/unidades", "/css/**", "/js/**", "/images/**", "/webjars/**")
                 .permitAll()  // Permite acceso a estas rutas sin autenticación
                 .anyRequest().authenticated()  // Requiere autenticación para el resto
             .and()
