a2b102435ea0d312e3dcc4fd7572d1760f043fed
noealas53 <noealas53@gmail.com>
2024-11-18 07:57:33 -0600

    Enhance loading indicators and error handling in clinic management functions

--- src/main/java/com/nikolas/leaflet/repository/ClinicaComunalRepository.java
+++ src/main/java/com/nikolas/leaflet/repository/ClinicaComunalRepository.java
@@ -1,13 +1,10 @@
 package com.nikolas.leaflet.repository;
 
 import com.nikolas.leaflet.domain.ClinicaComunal;
-import com.nikolas.leaflet.domain.UnidadMedica;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.Optional;
 import java.util.List;
 import org.springframework.data.domain.Page;
